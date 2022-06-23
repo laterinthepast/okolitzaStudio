f260240de4621fa4c0c0c37da0c3ca8c0e81a3fa
onlinefred <67547195+laterinthepast@users.noreply.github.com>
2022-06-23 21:39:38 +0100

    added metat to head

--- index.js
+++ index.js
@@ -39,10 +39,3 @@ hamburger.addEventListener('click', () => {
 }
 );
 
-
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
