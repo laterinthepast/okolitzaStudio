8fb560014569021b4abc9c3bc38e9e3c927f5cab
laterinthepast <67547195+laterinthepast@users.noreply.github.com>
2023-06-22 15:01:34 +0100

    another try

--- index.js
+++ index.js
@@ -70,23 +70,3 @@ window.addEventListener('load', () => {
     loaderContainer.style.display = 'none';
 });
 
-window.onload = () => {
-  // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
-  let all = document.getElementsByClassName("zoomD"),
-      lightbox = document.getElementById("lightbox");
- 
-  // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
-  // * SIMPLY CLONE INTO LIGHTBOX & SHOW
-  if (all.length>0) { for (let i of all) {
-    i.onclick = () => {
-      let clone = i.cloneNode();
-      clone.className = "";
-      lightbox.innerHTML = "";
-      lightbox.appendChild(clone);
-      lightbox.className = "show";
-    };
-  }}
- 
-  // (C) CLICK TO CLOSE LIGHTBOX
-  lightbox.onclick = () => lightbox.className = "";
-};
